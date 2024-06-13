7a705212d8fc5e45f68b8a05c1f33e018efbf378
jinyK <111173565+golden-dust@users.noreply.github.com>
2024-06-13 15:52:18 +0900

    Update 07_sidebar.py
    sign in feature available

--- 07_sidebar.py
+++ 07_sidebar.py
@@ -1,7 +1,6 @@
 import streamlit as st
 from PIL import Image
 
-
 # 사이드바 화면
 st.sidebar.title("sidebar")
 st.sidebar.header("type something")
@@ -11,24 +10,26 @@ user_id = st.sidebar.text_input("ID: ",
 user_pw = st.sidebar.text_input("PW: ", 
                                 placeholder="1234", 
                                 type="password")
-st.sidebar.header("Select Box")
 
-sel_opt = ["Girl with A Pearl Earring", 
-           "The Starry Night", 
-           "The Scream", 
-           "월하정인"]
-user_opt = st.sidebar.selectbox("Which picture do you like the most?", 
-                                sel_opt)
-st.sidebar.write("You selected: ", user_opt)
+st.title("Welcome to Streamlit Sidebar!")
+
+if (user_id == "streamlit") & (user_pw == "1234"):
+    st.sidebar.header("Select Box")
+    sel_opt = ["Girl with A Pearl Earring", 
+            "The Starry Night", 
+            "The Scream", 
+            "월하정인"]
+    user_opt = st.sidebar.selectbox("Which picture do you like the most?", 
+                                    sel_opt)
+    st.sidebar.write("You selected: ", user_opt)
+        
+    # 메인 화면
+    st.header("Cool Paintings :sunglasses:")
+    folder = "data/"
+    image_files = ["Vermeer.png", "Gogh.png", "Munch.png", "ShinYoonbok.png"]
 
 
-# 메인 화면
-st.title("Streamlit Sidebar")
-
-image_files = ["Vermeer.png", "Gogh.png", "Munch.png", "ShinYoonbok.png"]
-
-
-i = sel_opt.index(user_opt)
-img_path = image_files[i]
-img = Image.open(img_path)
-st.image(img, caption=user_opt)
+    i = sel_opt.index(user_opt)
+    img_path = folder + image_files[i]
+    img = Image.open(img_path)
+    st.image(img, caption=user_opt)
