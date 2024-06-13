3f7d31528e24766cd09c400fd686668925804607
jinyK <111173565+golden-dust@users.noreply.github.com>
2024-06-13 15:20:17 +0900

    initial commit

---
+++ 07_sidebar.py
@@ -0,0 +1,35 @@
+import streamlit as st
+from PIL import Image
+
+
+# 사이드바 화면
+st.sidebar.title("sidebar")
+st.sidebar.header("type something")
+user_id = st.sidebar.text_input("ID: ", 
+                                placeholder="streamlit", 
+                                max_chars=16, )
+user_pw = st.sidebar.text_input("PW: ", 
+                                placeholder="1234", 
+                                type="password")
+st.sidebar.header("Select Box")
+
+sel_opt = ["Girl with A Pearl Earring", 
+           "The Starry Night", 
+           "The Scream", 
+           "월하정인"]
+user_opt = st.sidebar.selectbox("Which picture do you like the most?", 
+                                sel_opt)
+st.sidebar.write("You selected: ", user_opt)
+
+
+# 메인 화면
+st.title("Streamlit Sidebar")
+
+folder = "data/"
+image_files = ["Vermeer.png", "Gogh.png", "Munch.png", "ShinYoonbok.png"]
+
+
+i = sel_opt.index(user_opt)
+img_path = folder + image_files[i]
+img = Image.open(img_path)
+st.image(img, caption=user_opt)
