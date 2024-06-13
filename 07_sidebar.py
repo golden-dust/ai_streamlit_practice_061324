831b61c9ba6db6f99bcb773fee477f4db00ca187
jinyK <111173565+golden-dust@users.noreply.github.com>
2024-06-13 16:59:55 +0900

    Add login and logout feature

--- 07_sidebar.py
+++ 07_sidebar.py
@@ -1,19 +1,23 @@
 import streamlit as st
 from PIL import Image
 
+greeting = st.title("Welcome to Streamlit Sidebar!")
+
 # 사이드바 화면
 st.sidebar.title("sidebar")
 st.sidebar.header("type something")
-user_id = st.sidebar.text_input("ID: ", 
+username = st.sidebar.text_input("ID: ", 
                                 placeholder="streamlit", 
                                 max_chars=16, )
-user_pw = st.sidebar.text_input("PW: ", 
+password = st.sidebar.text_input("PW: ", 
                                 placeholder="1234", 
                                 type="password")
 
-st.title("Welcome to Streamlit Sidebar!")
 
-if (user_id == "streamlit") & (user_pw == "1234"):
+def signed_in():
+    greeting.empty()
+    st.sidebar.empty()
+    logout_btn = st.button("logout")
     st.sidebar.header("Select Box")
     sel_opt = ["Girl with A Pearl Earring", 
             "The Starry Night", 
@@ -24,12 +28,26 @@ if (user_id == "streamlit") & (user_pw == "1234"):
     st.sidebar.write("You selected: ", user_opt)
         
     # 메인 화면
-    st.header("Cool Paintings :sunglasses:")
+    st.title("Streamlit Sidebar")
+    st.subheader("Cool Paintings :sunglasses:")
     folder = "data/"
     image_files = ["Vermeer.png", "Gogh.png", "Munch.png", "ShinYoonbok.png"]
-
 
     i = sel_opt.index(user_opt)
     img_path = folder + image_files[i]
     img = Image.open(img_path)
     st.image(img, caption=user_opt)
+    
+    
+    if logout_btn:
+        st.toast("Logged out!")
+
+
+signin_btn = st.sidebar.button("sign in")
+
+if signin_btn:
+        if (username == "streamlit") & (password == "1234"):
+            st.toast(f"Hello, {username}")
+            signed_in()
+        else:
+            st.error("Invalid username or password")
